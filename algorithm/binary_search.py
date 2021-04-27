6b4cf48d74ac0a95fe45688aad20357d89a43de5
binogood <binogood68@gmail.com>
2021-04-27 14:02:54 +0900

    binary search algorithm

--- algorithm/binary_search.py
+++ algorithm/binary_search.py
@@ -1,13 +1,18 @@
 
-# 선형 탐색 알고리즘 
-# 처음 부터 하나씩 확인
+# 이진 탐색 알고리즘
+# 정렬된 리스트에서 절반씩 탐색
 
 def linear_search(element, some_list):
-    for idx in some_list:
-        if idx == element:
-            return some_list.index(element)
-    return None
-
+    start_idx = 0 
+    end_idx = len(some_list) - 1
+    while start_idx <= end_idx:
+        mid_idx = (end_idx + start_idx) // 2
+        if some_list[mid_idx] == element:
+            return mid_idx
+        elif some_list[mid_idx] > element:
+            end_idx = mid_idx - 1
+        else:
+            start_idx = mid_idx + 1
 
 print(linear_search(2, [2, 3, 5, 7, 11]))
 print(linear_search(0, [2, 3, 5, 7, 11]))
